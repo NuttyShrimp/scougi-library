69cf28dd9fbcfd9a1d2812bb25b685251979bdd7
NuttyShrimp <jan.lecoutere@hotmail.be>
2022-09-20 12:22:24 +0200

    fix(download): added logging for finding error

--- src/pages/api/scougi/download.ts
+++ src/pages/api/scougi/download.ts
@@ -1,17 +1,17 @@
 import { NextApiRequest, NextApiResponse } from "next";
 import { PDFDocument } from "pdf-lib";
-import { Readable } from "stream";
+import { log } from "next-axiom";
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
-  if (req.method !== 'GET') {
+  if (req.method !== "GET") {
     res.setHeader("Allow", ["GET"]);
     res.status(405).end(`Method ${req.method} Not Allowed`);
     return;
   }
 
-  const { id } = req.query
+  const { id } = req.query;
   if (!id || Number.isNaN(Number(id))) {
-    return res.status(400).send({ message: "missing data" })
+    return res.status(400).send({ message: "missing data" });
   }
 
   const pages = await prisma?.scougiPdfPage.findMany({
@@ -19,22 +19,25 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
       id: Number(id),
     },
     orderBy: {
-      number: 'asc'
-    }
-  })
+      number: "asc",
+    },
+  });
   if (!pages) {
     return res.status(404).send({});
   }
+  log.info("Started downloading process", { id });
+
   const mergedPDF = await PDFDocument.create();
   for (const pageData of pages) {
     const pagePDF = await PDFDocument.load(new Uint8Array(pageData.data));
     const [page] = await mergedPDF.copyPages(pagePDF, [0]);
-    mergedPDF.addPage(page)
+    mergedPDF.addPage(page);
   }
 
-  const pdfBuffer = Buffer.from(await mergedPDF.save())
+  log.info("Successfully combined PDF", { id });
+  const pdfBuffer = Buffer.from(await mergedPDF.save());
 
-  res.setHeader('Content-Type', 'application/pdf');
-  res.setHeader('Content-Disposition', "attachment; filename=scougi.pdf");
-  res.status(200).send(pdfBuffer)
+  res.setHeader("Content-Type", "application/pdf");
+  res.setHeader("Content-Disposition", "attachment; filename=scougi.pdf");
+  res.status(200).send(pdfBuffer);
 }
