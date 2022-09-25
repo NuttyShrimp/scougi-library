e9f4c9d28387ef9a23ce4628d897c3bab9b2dee4
NuttyShrimp <jan.lecoutere@hotmail.be>
2022-09-25 10:22:51 +0200

    feat(downloade): catch errors

--- src/pages/api/scougi/download.ts
+++ src/pages/api/scougi/download.ts
@@ -14,42 +14,49 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
     return;
   }
 
-  const { id } = req.query;
-  if (!id || Number.isNaN(Number(id))) {
-    return res.status(400).send({ message: "missing data" });
+  try {
+    const { id } = req.query;
+    if (!id || Number.isNaN(Number(id))) {
+      return res.status(400).send({ message: "missing data" });
+    }
+    const logger = log.with({ handle: "download", id });
+    logger.info("Received a download request");
+
+    const pages = await prisma.scougiPdfPage.findMany({
+      where: {
+        id: Number(id),
+      },
+      orderBy: {
+        number: "asc",
+      },
+    });
+    if (!pages) {
+      return res.status(404).send({ message: "No pages for scougi" });
+    }
+    logger.info("Started downloading process", { id });
+
+    const mergedPDF = await PDFDocument.create();
+    for (const pageData of pages) {
+      const pagePDF = await PDFDocument.load(new Uint8Array(pageData.data));
+      const [page] = await mergedPDF.copyPages(pagePDF, [0]);
+      mergedPDF.addPage(page);
+    }
+    const pdfBytes = await mergedPDF.save();
+    const isProd = process.env.NODE_ENV === "production";
+    const pdfPath = path.join(isProd ? process.cwd() : os.tmpdir(), "scougi.pdf");
+    writeFileSync(pdfPath, pdfBytes);
+
+    log.info("Successfully combined PDF", { id });
+
+    const pdfReadBuffer = readFileSync(pdfPath);
+
+    res.setHeader("Content-Type", "application/pdf");
+    // res.setHeader("Content-Disposition", "scougi.pdf");
+    return res.end(pdfReadBuffer);
+  } catch (e) {
+    console.error(e);
+    return res.setHeader("Content-Type", "application/json").status(500).json({
+      error: e,
+    });
   }
-  const logger = log.with({ handle: "download", id });
-  logger.info("Received a download request");
-
-  const pages = await prisma.scougiPdfPage.findMany({
-    where: {
-      id: Number(id),
-    },
-    orderBy: {
-      number: "asc",
-    },
-  });
-  if (!pages) {
-    return res.status(404).send({ message: "No pages for scougi" });
-  }
-  logger.info("Started downloading process", { id });
-
-  const mergedPDF = await PDFDocument.create();
-  for (const pageData of pages) {
-    const pagePDF = await PDFDocument.load(new Uint8Array(pageData.data));
-    const [page] = await mergedPDF.copyPages(pagePDF, [0]);
-    mergedPDF.addPage(page);
-  }
-  const pdfBytes = await mergedPDF.save();
-  const isProd = process.env.NODE_ENV === "production";
-  const pdfPath = path.join(isProd ? process.cwd() : os.tmpdir(), "scougi.pdf");
-  writeFileSync(pdfPath, pdfBytes);
-
-  log.info("Successfully combined PDF", { id });
-
-  const pdfReadBuffer = readFileSync(pdfPath);
-
-  res.setHeader("Content-Type", "application/pdf");
-  // res.setHeader("Content-Disposition", "scougi.pdf");
-  return res.end(pdfReadBuffer);
 }
