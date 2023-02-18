e18fbc20ca4f78beae24e0fba0f7c0a7de90aa3b
Jan Lecoutere <jan.lecoutere@hotmail.be>
2023-02-18 13:50:52 +0100

    fix(root): remove plausible script

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -16,7 +16,6 @@ const queryClient = new QueryClient()
 
 import "@fortawesome/fontawesome-svg-core/styles.css";
 import { config } from "@fortawesome/fontawesome-svg-core";
-import Script from "next/script";
 config.autoAddCss = false; // Tell Font Awesome to skip adding the CSS automatically since it's being imported above
 
 function MyApp({ Component, pageProps: { session, ...pageProps } }: AppProps) {
@@ -28,7 +27,6 @@ function MyApp({ Component, pageProps: { session, ...pageProps } }: AppProps) {
           <meta name="description" content="Verzameling van alle beschibare scougis van Scouts en Gidsen Asse" />
           <link rel="shortcut icon" href="/favicon.ico" />
         </Head>
-        <Script data-domain="scougi-library.vercel.app" src="https://plausible.nuttyshrimp.me/js/plausible.js" />
         <MantineProvider withGlobalStyles withNormalizeCSS theme={theme}>
           <QueryClientProvider client={queryClient}>
             <ModalsProvider>
