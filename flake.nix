7a3d19a882d74a7c5ee62d064d197032543be618
NuttyShrimp <jan.lecoutere@hotmail.be>
2024-01-30 00:54:17 +0100

    Initial commit from Create Next App

---
+++ flake.nix
@@ -0,0 +1,26 @@
+{
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+    devshell = {
+      url = "github:numtide/devshell";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+
+  outputs = { flake-utils, nixpkgs, devshell, ... }:
+    flake-utils.lib.eachDefaultSystem (system: 
+      let
+        pkgs = import nixpkgs {
+          inherit system;
+
+          overlays = [ devshell.overlays.default ];
+        };
+      in
+      with pkgs; {
+        devShells.default = pkgs.devshell.mkShell {
+          packages = [ nodejs nodePackages.pnpm ];
+        };
+      }
+    );
+}
