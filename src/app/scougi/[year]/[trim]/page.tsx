32d0d5bb482e890db74c65a73c55707c4cc2b5fc
NuttyShrimp <jan.lecoutere@hotmail.be>
2024-02-13 22:19:10 +0100

    chore: cleanup

--- src/app/scougi/[year]/[trim]/page.tsx
+++ src/app/scougi/[year]/[trim]/page.tsx
@@ -1,8 +1,3 @@
-import { PageFlipper } from "@/components/PageFlipper";
-import { TrimesterNames } from "@/enums/trimesterNames";
-import db from "@/lib/db";
-import { ArrowLeft } from "lucide-react";
-import Link from "next/link";
 import { redirect } from "next/navigation";
 
 export default async function Page({ params }: { params: { year: string; trim: number; } }) {
