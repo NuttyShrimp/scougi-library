e94c1ed976514d52ce8677b73ab05acffde9551a
NuttyShrimp <jan.lecoutere@hotmail.be>
2024-04-13 10:19:36 +0200

    chore: add logos

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,16 @@
 import { ScougiList, ScougiListSkeleton } from "@/components/ScougiList";
+import Image from "next/image";
 import { Suspense } from "react";
 
 export default async function Page() {
   return (
     <div className="w-screen">
-      <div className="flex flex-col justify-center items-center">
-        <h1 className="mb-3">
-          Scougi - Scouts en Gidsen Asse
-        </h1>
-        <h3 className="mt-3">Lees hier uw favoriete trimestriële blad!</h3>
+      <div className="flex flex-col items-center sm:items-start container bg-zinc-400 mx-auto px-20 py-2 not-prose">
+        <Image src="/scougi.png" alt="Scougi" width={128} height={32} className="w-32" />
+        <div className="flex flex-row items-center">
+          <p className="text-gray-100">by</p>
+          <Image src="/logo.png" alt="Scouts & Gidsen Asse" width={48} height={48} className="h-12" />
+        </div>
       </div>
       <Suspense fallback={<ScougiListSkeleton />}>
         <ScougiList />
