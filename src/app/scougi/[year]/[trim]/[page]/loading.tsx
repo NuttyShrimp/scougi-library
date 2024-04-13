ee03873f523b39fb8375dce907082ea3709be8d9
NuttyShrimp <jan.lecoutere@hotmail.be>
2024-04-13 11:29:48 +0200

    chore: update deps

--- src/app/scougi/[year]/[trim]/[page]/loading.tsx
+++ src/app/scougi/[year]/[trim]/[page]/loading.tsx
@@ -1,8 +1,8 @@
-export default function Loading(){
+export default function Loading() {
   return (
     <div className="p-2">
       <div className="mx-auto w-fit mt-2">
-        <div className="skeleton w-[30vw] h-[80svh]"></div>
+        <div className="skeleton w-[80vw] sm:w-[30vw] h-[80svh]"></div>
         <div className="skeleton w-36 h-8 mt-4 mx-auto"></div>
       </div>
     </div>
