e467ad68588ee94a153b8fb4df8561d9a2117386
NuttyShrimp <jan.lecoutere@hotmail.be>
2024-04-13 10:06:50 +0200

    chore: make scougipages bigger on home site

--- src/components/ScougiList.tsx
+++ src/components/ScougiList.tsx
@@ -34,10 +34,10 @@ export const ScougiListSkeleton = async () => {
     <div className="mx-auto container flex flex-col gap-2 items-center">
       <div className="skeleton w-32 h-8"></div>
       <div className="flex justify-evenly gap-4 w-full">
-        <div className="skeleton w-16 h-24"></div>
-        <div className="skeleton w-16 h-24"></div>
-        <div className="skeleton w-16 h-24"></div>
-        <div className="skeleton w-16 h-24"></div>
+        <div className="skeleton w-24 h-32"></div>
+        <div className="skeleton w-24 h-32"></div>
+        <div className="skeleton w-24 h-32"></div>
+        <div className="skeleton w-24 h-32"></div>
       </div>
     </div>
   )
--- src/components/YearShelf.tsx
+++ src/components/YearShelf.tsx
@@ -15,14 +15,14 @@ export const YearShelf = async (props: { year: string, trims: number[] }) => {
   return (
     <div>
       <h2 className="text-center my-4">{props.year}</h2>
-      <div className="border border-gray-200 rounded p-2 flex h-24 gap-4 justify-evenly sm:mx-0 mx-2">
+      <div className="border border-gray-200 rounded p-2 flex h-32 gap-4 justify-evenly sm:mx-0 mx-2">
         {props.trims.map((t, i) => {
           if (t === null) return null;
           let data = trimFirstPages.find(p => p.id === t);
           if (!data) return null;
           return (
             <Link key={`${props.year}-${data.id}`} href={`/scougi/${props.year}/${i}`} >
-              <ScougiPage data={data.data} height={80} />
+              <ScougiPage data={data.data} height={110} />
             </Link>
           )
         })}
