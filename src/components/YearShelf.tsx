a948317572724e8eb52ee1ea394176dca413d0c2
NuttyShrimp <jan.lecoutere@hotmail.be>
2024-04-27 15:21:38 +0200

    refactor: do yearshelf fetch with select instead of query

--- src/components/YearShelf.tsx
+++ src/components/YearShelf.tsx
@@ -5,27 +5,27 @@ import { and, eq, inArray } from "drizzle-orm";
 import { ScougiPageTable } from "@/lib/db/schema";
 
 export const YearShelf = async (props: { year: string, trims: number[] }) => {
-  const trimFirstPages = await db.query.ScougiPageTable.findMany({
-    columns: {
-      id: true,
-      data: true,
-    },
-    where: and(eq(ScougiPageTable.number, 0), inArray(ScougiPageTable.id, props.trims.filter(t => t)))
-  });
+  const trimFirstPages = await db.select({
+    id: ScougiPageTable.id,
+    data: ScougiPageTable.data,
+  }).from(ScougiPageTable).where(and(eq(ScougiPageTable.number, 0), inArray(ScougiPageTable.id, props.trims.filter(t => t))));
+
   return (
     <div>
-      <h2 className="text-center my-4">{props.year}</h2>
-      <div className="border border-gray-200 rounded p-2 flex h-32 gap-4 justify-evenly sm:mx-0 mx-2">
-        {props.trims.map((t, i) => {
-          if (t === null) return null;
-          let data = trimFirstPages.find(p => p.id === t);
-          if (!data) return null;
-          return (
-            <Link key={`${props.year}-${data.id}`} href={`/scougi/${props.year}/${i}`} >
-              <ScougiPage data={data.data} height={110} static />
-            </Link>
-          )
-        })}
+      <h2 className="text-center my-4" > {props.year} </h2>
+      <div className="border border-gray-200 rounded p-2 flex h-32 gap-4 justify-evenly sm:mx-0 mx-2" >
+        {
+          props.trims.map((t, i) => {
+            if (t === null) return null;
+            let data = trimFirstPages.find(p => p.id === t);
+            if (!data) return null;
+            return (
+              <Link key={`${props.year}-${data.id}`
+              } href={`/scougi/${props.year}/${i}`} >
+                <ScougiPage data={data.data} height={110} static />
+              </Link>
+            )
+          })}
       </div>
     </div>
   )
